edb62b8497b28ea9a7c7911ed37154a505328b21
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2022-06-13 18:02:29 -0500

    ultimos cambios

--- app/src/main/java/com/ean/tienda_libros/mostrar_libros.kt
+++ app/src/main/java/com/ean/tienda_libros/mostrar_libros.kt
@@ -3,11 +3,10 @@ package com.ean.tienda_libros
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.ListView
 import Mundo.LaTienda.mostrar_todos_los_nombres_libros
-import android.widget.Toast
+import android.widget.*
+import androidx.core.view.contains
+import java.util.*
 
 class mostrar_libros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +20,27 @@ class mostrar_libros : AppCompatActivity() {
         }
         //lista
         val lista_de_libros=findViewById<ListView>(R.id.list_libros_mostrar)
+        val buscar=findViewById<SearchView>(R.id.SeatchView)
         val libros= mostrar_todos_los_nombres_libros()
         val adaptlibros= ArrayAdapter(this,android.R.layout.simple_list_item_1,libros)
         lista_de_libros.adapter=adaptlibros
+        buscar.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                buscar.clearFocus()
+                if(lista_de_libros.contains()){
 
+                }
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        })
         lista_de_libros.setOnItemClickListener(){parent,view,position,id->
             Toast.makeText(this,parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show()
         }
     }
 }
+
+
